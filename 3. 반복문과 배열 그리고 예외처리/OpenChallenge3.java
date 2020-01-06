10eaca09b258e6c3aaf42a2fd1b842135dca0c0b
witheunjin <sue9897@naver.com>
2020-01-06 17:26:33 +0900

    delete test files

--- 3. 반복문과 배열 그리고 예외처리/OpenChallenge3.java
+++
@@ -1,9 +0,0 @@
-package Chapter03;
-
-class OpenChallenge3
-{
-  public static void main(String[] args)
-  {
-    
-  }
-}
--- 3. 반복문과 배열 그리고 예외처리/실습문제/Problem02.java
+++
@@ -1,9 +0,0 @@
-package practical;
-
-class Problem02
-{
-  public static void main(String[] args)
-  {
-    
-  }
-}

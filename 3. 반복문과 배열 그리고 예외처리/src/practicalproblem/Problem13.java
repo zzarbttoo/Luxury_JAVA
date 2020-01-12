0dac1f73f93fbb3b0afc47ada393d0e94c2c0554
zzarbttoo <qodbwls064@gmail.com>
2020-01-12 15:28:01 +0900

    Do odd number exams in chapter3 except Problem11.

--- 3. 반복문과 배열 그리고 예외처리/src/practicalproblem/Problem13.java
+++ 3. 반복문과 배열 그리고 예외처리/src/practicalproblem/Problem13.java
@@ -4,24 +4,17 @@ public class Problem13 {
     public static void main(String[] args) {
 
         for (int i = 1; i < 100; i++) {
-            if(i<10){
-                if(i%3==0){
-                    System.out.println( i + " 박수 " + "짝");
+
+            if (i < 10) {
+                if (i % 3 == 0) {
+                    System.out.println(i + " 박수 " + "짝");
                 }
-            }
-            else{
-                if(Math.floor(i/10)%3==0 && (i%10)%3!=0){
-                    System.out.println( i + " 박수 " + "짝");
-                }
-                else if(Math.floor(i/10)%3!=0 && (i%10)%3==0){
-                    System.out.println( i + " 박수 " + "짝");
-                }
-                else if(i%10==0 && i%3==0){
-                    System.out.println( i + " 박수 " + "짝");
-                }
-                else if(Math.floor(i/10)%3==0 && (i%10)%3==0){
-                    System.out.println( i + " 박수 " + "짝짝");
-                }
+            } else if (Math.floor(i / 10) % 3 == 0 && i % 10 == 0) {
+                System.out.println(i + " 박수 " + "짝");
+            } else if (Math.floor(i / 10) % 3 == 0 && (i % 10) % 3 == 0) {
+                System.out.println(i + " 박수 " + "짝짝");
+            } else if (Math.floor(i / 10) % 3 != 0 && (i % 10) % 3 == 0 && i % 10 != 0){
+                System.out.println(i + " 박수 " + "짝");
             }
         }
 
